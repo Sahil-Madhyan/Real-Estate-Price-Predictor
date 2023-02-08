9d6f1ac559159563b7a7c03e1a238809229bfcbd
Sahil Madhyan <sahilmadhyan880@gmail.com>
2023-02-08 22:49:41 +0530

    Updated Files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import pandas as pd
 
-app = Flask(__name__,template_folder="docs",static_folder="./docs/static")
+app = Flask(__name__,template_folder="docs",static_folder="static")
 data = pd.read_csv("./excel_sheets/Delhi.csv")
 
 
